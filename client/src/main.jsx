defa68824bb1b23bf04b19b3274d5eb9809afdf7
jairvides <vides.jair@gmail.com>
2026-05-19 01:55:30 -0500

    Fix: update backend URL for production

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './index.css'
 import App from './App.jsx'
 
-axios.defaults.baseURL = 'https://payrollresume-production.up.railway.app';
+axios.defaults.baseURL = 'https://serene-endurance-production-2b93.up.railway.app';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
